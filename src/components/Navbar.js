cefda9dceae30954b60fca922dbd126de2f1c01c
Ishan <is.ishan.singla@gmail.com>
2022-08-03 06:45:33 +0530

    ishan

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { BrowserRouter } from "react-router-dom";
-
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState("home");
@@ -26,73 +24,69 @@ export const NavBar = () => {
   };
 
   return (
-    <BrowserRouter>
-      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-        <Container>
-          <Navbar.Brand href="/" size={12} sm={6}>
-            <img
-              style={{ height: 70, width: 70 }}
-              src="https://avatars.githubusercontent.com/u/79676239?v=4"
-              alt="Logo"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link
-                href="#home"
-                className={
-                  activeLink === "home" ? "active navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("home")}
-              >
-                Home
-              </Nav.Link>
-              <Nav.Link
-                href="#skills"
-                className={
-                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("skills")}
-              >
-                Skills
-              </Nav.Link>
-              <Nav.Link
-                href="#projects"
-                className={
-                  activeLink === "projects"
-                    ? "active navbar-link"
-                    : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("projects")}
-              >
-                Projects
-              </Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-              <div className="social-icon">
-                <a href="https://www.linkedin.com/in/isishansingla/">
-                  <img
-                    src="https://img.icons8.com/fluency/344/linkedin-2.png"
-                    alt=""
-                  />
-                </a>
-                <a href="https://github.com/IshanSingla">
-                  <img
-                    src="https://img.icons8.com/ios-filled/344/github.png"
-                    alt=""
-                  />
-                </a>
-              </div>
-              <button className="vvd">
-                <span>Let’s Connect</span>
-              </button>
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </BrowserRouter>
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="/" size={12} sm={6}>
+          <img
+            style={{ height: 70, width: 70 }}
+            src="https://avatars.githubusercontent.com/u/79676239?v=4"
+            alt="Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/isishansingla/">
+                <img
+                  src="https://img.icons8.com/fluency/344/linkedin-2.png"
+                  alt=""
+                />
+              </a>
+              <a href="https://github.com/IshanSingla">
+                <img
+                  src="https://img.icons8.com/ios-filled/344/github.png"
+                  alt=""
+                />
+              </a>
+            </div>
+            <button className="vvd">
+              <span>Let’s Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
